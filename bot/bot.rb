6053c23d497a1038484356cd43b94bd75f5502c1
Przemysław Janowski <p.janowski@outlook.com>
2016-06-16 17:52:24 +0200

    more work on homeoffice

--- bot/bot.rb
+++ bot/bot.rb
@@ -1,17 +1,35 @@
 require 'slack-ruby-bot'
-require 'active_support'
 require 'pry'
 require 'chronic'
 
 class PongBot < SlackRubyBot::Bot
   HOMEOFFICE = {}
+  IGNORED = ['USLACKBOT']
 
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
+  command 'homeoffice check', 'homeoffice who' do |client, data, match|
+    client.say(text: "Najpierw podaj datę!", channel: data.channel) if match['expression'].nil?
+
+    date = Chronic.parse(match['expression'])
+    home_people = HOMEOFFICE[date].to_a
+    office_people = client.users.select{ |k, v| v["is_bot"] == false }.keys - IGNORED - home_people
+
+    if home_people.any?
+      home_people_names = home_people.map { |person| "<@#{person}>"}.join(', ')
+      office_people_names = office_people.map { |person| "<@#{person}>"}.join(', ')
+      client.say(text: "#{date.to_date} z domu pracują: #{home_people_names}", channel: data.channel)
+      client.say(text: "W biurze powinni być: #{office_people_names}, inaczej wpierdziel", channel: data.channel)
+    else
+      client.say('text': "#{date.to_date} wszyscy są w biurze! (taa...)", channel: data.channel)
+    end
   end
 
-  command 'homeoffice' do |client, data, match|
-    client.say(text: "Hello <@#{data.user}>, #{match['expression']}", channel: data.channel)
+  command 'homeoffice at', 'homeoffice', 'homeoffice add' do |client, data, match|
+    client.say(text: "Najpierw podaj datę!", channel: data.channel) if match['expression'].nil?
+
+    date = Chronic.parse(match['expression'])
+    HOMEOFFICE[date] = [] if HOMEOFFICE[date].blank?
+    HOMEOFFICE[date] << data.user unless HOMEOFFICE[date].include?(data.user)
+    client.say(text: "#{date.to_date} pracujesz z domu!", channel: data.channel)
   end
 end
 
