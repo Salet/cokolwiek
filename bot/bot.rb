84a65b5d6a324224c4aeb26f01b1d98fd0d99c83
Przemysław Janowski <p.janowski@outlook.com>
2016-06-16 19:10:01 +0200

    help emssages

--- bot/bot.rb
+++ bot/bot.rb
@@ -5,26 +5,23 @@ require_relative 'homeoffice_manager'
 class PongBot < SlackRubyBot::Bot
   command 'help' do |client, data, match|
     client.say(text:
-    'Work days manager
-      Holidays
-        request holidays and list all holidays that have been already confirmed
+    'Exlabs office manager. Usage:
 
-        holiday
-          Request holiday
-          specify start and end date like so: urlop YYYY-MM-DD YYYY-MM-DD
+    *Holidays* - request holidays and list all holidays that have already been confirmed
+    `holiday <date> <date>`
+      Request holiday between given dates. Example `holiday 30-06-16 07-07-16`
 
-        holidays
-          Lists all confirmed holiday breaks
+    `holidays`, `holidays <date>`
+      Lists all confirmed holiday breaks, or check who is on holiday at a given date.
 
-      Office
-        specify when you are gonna be in the office and check who else will be there
 
-        homeoffice check, homeoffice who
-          Lists users that are gonna be in the office on given date
+    *Home Office* - specify when are you going to be working from home and who will be in the office at a given date
+    `homeoffice check <date>`, `homeoffice who <date>`
+      Lists users that are going to be working from home at a given date.
 
-        homeoffice at, homeoffice, homeoffice add
-          Sets you as working from home on a given date
-          ',channel: data.channel)
+    `homeoffice <date>`, `homeoffice at <date>`, `homeoffice add <date>`
+      Sets you as working from home on a given date.
+      ', channel: data.channel)
   end
 
   include HolidaysManager
