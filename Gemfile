3aa771297705da82a3abe3691b4d907a4ffa9a5f
Przemysław Janowski <p.janowski@outlook.com>
2016-06-16 16:41:32 +0200

    work on homeoffice command

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'slack-ruby-bot'
 gem 'celluloid-io'
+gem 'pry'
+gem 'activesupport'
+gem 'chronic'
--- bot/bot.rb
+++ bot/bot.rb
@@ -1,8 +1,17 @@
 require 'slack-ruby-bot'
+require 'active_support'
+require 'pry'
+require 'chronic'
 
 class PongBot < SlackRubyBot::Bot
+  HOMEOFFICE = {}
+
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
+  end
+
+  command 'homeoffice' do |client, data, match|
+    client.say(text: "Hello <@#{data.user}>, #{match['expression']}", channel: data.channel)
   end
 end
 
