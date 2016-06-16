8f7255c9fd78219ec063c0262e648ae6b5471a2c
Protoplaste <protoplaste@gmail.com>
2016-06-16 19:16:04 +0200

    events manager

---
+++ bot/events_manager.rb
@@ -0,0 +1,29 @@
+require 'active_support'
+require 'chronic'
+
+module EventManager
+  extend ActiveSupport::Concern
+
+  @@events = []
+
+  included do
+
+    command 'events', 'current events' do |client, data, match|
+      if @@events.any?
+        @@events.each do |e|
+          text = "#{e[:name]} - #{e[:description]} learn more at #{e[:link]}"
+          client.say(text: text, channel: data.channel)
+        end
+      else
+        client.say(text: "No events planned", channel: data.channel)
+      end
+    end
+
+    command 'add event' do |client, data, match|
+      values = match['expression'].split('|')
+      keys = [:name, :description, :link]
+      @@events << Hash[*keys.zip(values).flatten]
+      client.say(text: "Event added", channel: data.channel)
+    end
+  end
+end
